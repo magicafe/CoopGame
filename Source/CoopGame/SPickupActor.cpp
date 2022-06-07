d671527ab0b4509561123ed86e078b9f5501a277
luodong <luod1027@gmail.com>
2022-06-07 09:47:15 +0800

    powerup

---
+++ Source/CoopGame/SPickupActor.cpp
@@ -0,0 +1,42 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SPickupActor.h"
+#include "Components/SphereComponent.h"
+#include "Components/DecalComponent.h"
+#include "SPowerupActor.h"
+
+// Sets default values
+ASPickupActor::ASPickupActor()
+{
+	SphereComp = CreateDefaultSubobject<USphereComponent>(TEXT("SphereComp"));
+	SphereComp->SetSphereRadius(75);
+	RootComponent = SphereComp;
+
+	DecalComp = CreateDefaultSubobject<UDecalComponent>(TEXT("DecalComp"));
+	DecalComp->SetRelativeRotation(FRotator(90, 0, 0));
+	DecalComp->DecalSize = FVector(64, 75, 75);
+	DecalComp->SetupAttachment(RootComponent);
+}
+
+// Called when the game starts or when spawned
+void ASPickupActor::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void ASPickupActor::Respawn()
+{
+	FActorSpawnParameters SpawnParams;
+	SpawnParams.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AlwaysSpawn;
+
+	GetWorld()->SpawnActor<ASPowerupActor>(PowerupClass, GetTransform(), SpawnParams);
+}
+
+void ASPickupActor::NotifyActorBeginOverlap(AActor* OtherActor)
+{
+	Super::NotifyActorBeginOverlap(OtherActor);
+}
+
+
---
+++ Source/CoopGame/SPowerupActor.cpp
@@ -0,0 +1,45 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SPowerupActor.h"
+
+// Sets default values
+ASPowerupActor::ASPowerupActor()
+{
+	PowerupInterval = 0.0f;
+	TotalNumOfTicks = 0;
+}
+
+// Called when the game starts or when spawned
+void ASPowerupActor::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	
+}
+
+void ASPowerupActor::OnTickPowerup()
+{
+	TicksProcessed++;
+
+	OnPowerupTicked();
+
+	if (TotalNumOfTicks >= TicksProcessed)
+	{
+		OnExpired();
+
+		GetWorldTimerManager().ClearTimer(TimerHandle_PowerupTick);
+	}	
+}
+
+void ASPowerupActor::ActivatePowerup()
+{
+	if (PowerupInterval > 0)
+	{
+		GetWorldTimerManager().SetTimer(TimerHandle_PowerupTick, this, &ASPowerupActor::OnTickPowerup, PowerupInterval, true, 0);
+	}
+	else
+	{
+		OnTickPowerup();
+	}
+}
